7537b274f25aa3da3b932e68a4d08a6ca672defa
Paul H. Müller <paul@adeven.com>
2013-08-18 18:06:08 +0200

    enable support for test -i flag
    this should speed up the ci tests a lot

--- goem/test.go
+++ goem/test.go
@@ -4,20 +4,35 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func test(config *Config, testDir string) bool {
 	setGoPath()
+	buildPackages := false
 
 	if testDir == "" {
 		testDir = config.Testdir
+	} else {
+		dirs := strings.Split(testDir, " ")
+		if len(dirs) > 1 && dirs[0] == "-i" {
+			buildPackages = true
+		}
+		testDir = dirs[len(dirs)-1]
 	}
 	os.Chdir(testDir)
+
+	testCommand := ""
+	if buildPackages {
+		testCommand = "-i"
+	}
 
 	cmd := exec.Command(
 		"go",
 		"test",
+		testCommand,
 	)
+
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("%s %s\n", out, err.Error())
